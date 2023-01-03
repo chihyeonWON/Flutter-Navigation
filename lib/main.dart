f8b7ed698ae0ac49e34bece6829bb515770f112e
ylose <heungeob1003@gmail.com>
2023-01-03 11:40:37 +0900

    이전 화면으로 데이터 돌려주기

--- lib/main.dart
+++ lib/main.dart
@@ -6,60 +6,53 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:'Flutter Demo',
-      theme:ThemeData(
+      title: 'Flutter Demo',
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:FirstPage(), // 첫 페이지를 시작 페이지로 지정
+      home: FirstPage(),   // 첫 페이지를 시작 페이지로 지정
     );
   }
-}
-
-class Person {
-  String name; // 이름
-  int age; // 나이
-
-  Person(this.name, this.age);
 }
 
 // 첫 페이지
 class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print('FirstPage build()');
     return Scaffold(
-      appBar:AppBar(
-        title:Text('First'),
+      appBar: AppBar(
+        title: Text('First'),
       ),
-      body:ElevatedButton(
-        child:Text('다음 페이지로'),
-        onPressed: (){
-          final person = Person('홍길동', 20);
-          Navigator.push(
+      body: ElevatedButton(
+        child: Text('다음 페이지로'),
+        onPressed: () async {
+          final result = await Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => SecondPage()),
           );
+
+          print(result);
         },
-      )
+      ),
     );
   }
 }
 
-// 두 번째 페이지
+
+// 두번째 페이지
 class SecondPage extends StatelessWidget {
-  final Person person;
-
-  SecondPage({@required this.person});
-
   @override
   Widget build(BuildContext context) {
+    print('SecondPage build()');
     return Scaffold(
-      appBar:AppBar(
-        title:Text('Second'),
+      appBar: AppBar(
+        title: Text('Second'),
       ),
-      body:ElevatedButton(
-        child:Text('이전 페이지로'),
-        onPressed:(){
-          Navigator.pop(context); // 현재 화면을 종료하고 이전 화면으로 돌아가기
+      body: ElevatedButton(
+        child: Text('이전 페이지로'),
+        onPressed: () {
+          Navigator.pop(context, 'ok');
         },
       ),
     );
